8722798c8a9fba73688ba7fea3820919bcb1afbe
Justin Yoo <justin.yoojh@gmail.com>
2022-03-24 23:08:25 +0900

    Update README

--- README.md
+++ README.md
@@ -5,5 +5,11 @@ This provides sample Azure Functions apps that integrate with Azure API Manageme
 
 ## Getting Started ##
 
-TBD
+1. Click the button below to autopilot all instances and apps on Azure. Make sure that you don't forget the app name you give.
 
+    [![Deploy To Azure](https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/1-CONTRIBUTION-GUIDE/images/deploytoazure.svg?sanitize=true)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fdevkimchi%2FAPIM-OpenAPI-Sample%2Fmain%2FResources%2Fazuredeploy.json)
+
+2. Visit the following URLs to check whether all the apps have been properly provisioned and deployed.
+
+   * `https://apim-<APP_NAME>.azure-api.net/azip/swagger/ui`
+   * `https://apim-<APP_NAME>.azure-api.net/azoop/swagger/ui`
---
+++ Resources/Purge-ApiManagement.ps1
@@ -0,0 +1,65 @@
+# Provisions resources based on Flags
+Param(
+    [string]
+    [Parameter(Mandatory=$false)]
+    $ResourceName = "",
+
+    [string]
+    [Parameter(Mandatory=$false)]
+    $Location = "westus2",
+
+    [string]
+    [Parameter(Mandatory=$false)]
+    $ApiVersion = "2021-08-01",
+
+    [switch]
+    [Parameter(Mandatory=$false)]
+    $Help
+)
+
+function Show-Usage {
+    Write-Output "    This permanently deletes the API Management instance
+
+    Usage: $(Split-Path $MyInvocation.ScriptName -Leaf) ``
+            -ResourceName <resource name> ``
+            [-Location <location>] ``
+            [-ApiVersion <API version>] ``
+
+            [-Help]
+
+    Options:
+        -ResourceName   Resource name.
+        -Location       Location. Default is `westus2`.
+        -ApiVersion     REST API version. Default is `2021-08-01`.
+
+        -Help:          Show this message.
+"
+
+    Exit 0
+}
+
+# Show usage
+$needHelp = ($ResourceName -eq "") -or ($Help -eq $true)
+if ($needHelp -eq $true) {
+    Show-Usage
+    Exit 0
+}
+
+$account = $(az account show | ConvertFrom-Json)
+
+$url = "https://management.azure.com/subscriptions/$($account.id)/providers/Microsoft.ApiManagement/locations/$($Location)/deletedservices/$($ResourceName)?api-version=$($ApiVersion)"
+
+# Uncomment to debug
+# $url
+
+Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Checking whether deleted API Management instance exists ..."
+$apim = $(az rest -m get -u $url)
+
+if ($apim -ne $null) {
+    Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Purging deleted API Management instance ..."
+    az rest -m delete -u $url --verbose
+
+    Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] Deleted API Management instance has been purged"
+} else {
+    Write-Output "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] No deleted API Management instance found to purge"
+}
