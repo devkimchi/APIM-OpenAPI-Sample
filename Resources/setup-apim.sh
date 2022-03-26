bbaa3eb2f48c26d03735fabc8592dfb277bddbda
Justin Yoo <justin.yoojh@gmail.com>
2022-03-27 00:04:57 +0900

    Update bicep files

--- Resources/setup-apim.sh
+++ Resources/setup-apim.sh
@@ -12,7 +12,7 @@ ipapp=$(az functionapp deploy -g rg-$RESOURCE_NAME -n fncapp-$RESOURCE_NAME-ip -
 oopapp=$(az functionapp deploy -g rg-$RESOURCE_NAME -n fncapp-$RESOURCE_NAME-ip --src-url $oopzip --type zip)
 
 # Provision APIs to APIM
-az deployment group create -n ApiManagement_Api -g rg-$RESOURCE_NAME -u https://raw.githubusercontent.com/devkimchi/APIM-OpenAPI-Sample/main/Resources/provision-apimanagementapi.bicep -p name=$RESOURCE_NAME
+az deployment group create -n ApiManagement_Api -g rg-$RESOURCE_NAME -u https://raw.githubusercontent.com/devkimchi/APIM-OpenAPI-Sample/main/Resources/provision-apimanagementapi.json -p name=$RESOURCE_NAME
 
 # Update app settings on function apps
 iphostnames=https://apim-$RESOURCE_NAME.azure-api.net/azip,$(az functionapp config appsettings list -g rg-$RESOURCE_NAME -n fncapp-$RESOURCE_NAME-ip | jq '.[] | select(.name == "OpenApi__HostNames") | .value' -r)
